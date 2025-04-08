3b1608170a2c0c96ef705b8ff6a195e0a836a0d1
MegaEmme <mikmusco@gmail.com>
2025-04-08 15:05:11 +0200

    feat: create useState

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
+
 function Button(props) {
     const buttons = props.Button;
+    const [press, setPress] = useState(null);
     return (
         <>
             {buttons.map(element =>
-                <button key={element.id} className="btn btn-primary mb-5 m-2">{element.title}</button>
+                <button onClick={() => { setPress(press === element.title) }} key={element.id} className="btn btn-primary mb-5 m-2">{`${element.title} ${press}`}</button>
             )}
         </>
     )
