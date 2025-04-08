b6865d0e9a3661f8400e7e9ac8d496eac07af599
MegaEmme <mikmusco@gmail.com>
2025-04-08 14:31:52 +0200

    feat: create Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+function Card(props) {
+    const cards = props.Card
+    return (
+        <>
+            {cards.map(element =>
+                <div key={element.id} className="card">
+                    <h4 className="card-body">{element.title}</h4>
+                    <p className="card-body">{element.description}</p>
+                </div>
+            )}
+        </>
+    )
+};
+
+export default Card;
