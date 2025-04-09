3def0d386432238593e1f49a2a1bee3dd1893938
MegaEmme <mikmusco@gmail.com>
2025-04-09 11:33:50 +0200

    feat: modified buffon

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,14 +5,14 @@ function Button(props) {
     const [isActive, setIsActive] = useState(null);
     return (
         <>
-            <div className={`m-2 mb-4 ${isActive === null ? 'd-block' : 'd-none'}`}>
-                <h4 className="card-body">NESSUN LINGUAGGIO SELEZIONATO</h4>
-            </div>
             {buttons.map(element =>
                 <button onClick={() => { setIsActive(element.title); props.changePress(element.title) }} key={element.id} className={`btn mb-5 m-2 ${isActive === element.title ? 'btn-warning' : 'btn-primary'}`}>
                     {element.title}
                 </button>
             )}
+            <div className={`card m-2 mb-4 ${isActive === null ? 'd-block' : 'd-none'}`}>
+                <h4 className="card-body">NESSUN LINGUAGGIO SELEZIONATO</h4>
+            </div>
         </>
     )
 };
